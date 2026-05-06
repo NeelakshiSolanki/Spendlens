50ca460b037c8d205e5a9a967903093e1b44551d
Neelakshi solanki <122295718+neelakshi-204@users.noreply.github.com>
2026-05-07 00:38:30 +0530

    Updates

--- spendlens/src/pages/Home.jsx
+++ spendlens/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import SpendForm from "../components/SpendForm"
 
 const Home = () => {
+
   return (
-    <div className="min-h-screen bg-black text-white p-10">
+    <div className="min-h-screen bg-black p-10">
+
       <SpendForm />
+
     </div>
   )
 }
